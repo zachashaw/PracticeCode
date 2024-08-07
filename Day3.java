248315af909d349f8dfe071b5db8e845c8458950
zachashaw <90483131+zachashaw@users.noreply.github.com>
2024-08-07 19:07:19 -0400

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,8 @@
+public class Day3 {
+    static int firstNum = 5;
+    final static String firstText = "salamander"; 
+  public static void main(String[] args) {
+    System.out.println(firstNum + firstText);
+    
+  }
+}
