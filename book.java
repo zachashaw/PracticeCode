59f4d8ce8eeaef0136ad4397b5fe7dfb59d43471
zachashaw <90483131+zachashaw@users.noreply.github.com>
2024-08-09 17:11:31 -0400

    Add files via upload

---
+++ book.java
@@ -0,0 +1,17 @@
+
+public class book {
+
+    String name = "Silmarillion";
+    String author = "Tolkien";
+    int pirce = 117;
+    String newName = "Hobbit";
+    boolean sold = false;
+
+    public void Selling() {
+        sold = true;
+    }
+
+    public void newName() {
+        name = newName;
+    }
+}
