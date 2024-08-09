02ae699cfbb07f4b6e76a4e347b6c0d7dce937f2
zachashaw <90483131+zachashaw@users.noreply.github.com>
2024-08-09 19:32:16 -0400

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,9 +1,21 @@
+
 public class day5 {
 
     int time = 40;
-
+    int timeInOven = 18;
     // In minutes
+    int layers = 8;
+
     public int expectedtiimeinoven() {
         return time;
     }
+
+    public int remainingMinutesInOven() {
+       return time - timeInOven;  
+    }
+    public int prepTimeInMinutes() {
+        return layers * 2;
+    }
+
+
 }
